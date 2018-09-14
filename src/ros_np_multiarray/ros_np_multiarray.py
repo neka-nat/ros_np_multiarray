2b9d0dceb3df8fa2c99d54af424f6dd087701b36
nekanat <nekanat.stock@gmail.com>
2018-09-14 20:46:02 +0900

    bug fix

--- src/ros_np_multiarray/ros_np_multiarray.py
+++ src/ros_np_multiarray/ros_np_multiarray.py
@@ -14,30 +14,30 @@ def _numpy_to_multiarray(multiarray_type, np_array):
     multiarray.data = np_array.reshape([1, -1])[0].tolist();
     return multiarray
 
-def _multiarray_to_numpy(dtype, multiarray):
+def _multiarray_to_numpy(pytype, dtype, multiarray):
     dims = map(lambda x: x.size, multiarray.layout.dim)
-    return np.array(multiarray.data, dtype=dtype).reshape(dims)
+    return np.array(multiarray.data, dtype=pytype).reshape(dims).astype(dtype)
 
 
 from functools import partial
 
 to_multiarray_f32 = partial(_numpy_to_multiarray, Float32MultiArray)
-to_numpy_f32 = partial(_multiarray_to_numpy, np.float32)
+to_numpy_f32 = partial(_multiarray_to_numpy, float, np.float32)
 to_multiarray_f64 = partial(_numpy_to_multiarray, Float64MultiArray)
-to_numpy_f64 = partial(_multiarray_to_numpy, np.float64)
+to_numpy_f64 = partial(_multiarray_to_numpy, float, np.float64)
 to_multiarray_i8 = partial(_numpy_to_multiarray, Int8MultiArray)
-to_numpy_i8 = partial(_multiarray_to_numpy, np.int8)
+to_numpy_i8 = partial(_multiarray_to_numpy, int, np.int8)
 to_multiarray_i16 = partial(_numpy_to_multiarray, Int16MultiArray)
-to_numpy_i16 = partial(_multiarray_to_numpy, np.int16)
+to_numpy_i16 = partial(_multiarray_to_numpy, int, np.int16)
 to_multiarray_i32 = partial(_numpy_to_multiarray, Int32MultiArray)
-to_numpy_i32 = partial(_multiarray_to_numpy, np.int32)
+to_numpy_i32 = partial(_multiarray_to_numpy, int, np.int32)
 to_multiarray_i64 = partial(_numpy_to_multiarray, Int64MultiArray)
-to_numpy_i64 = partial(_multiarray_to_numpy, np.int64)
+to_numpy_i64 = partial(_multiarray_to_numpy, int, np.int64)
 to_multiarray_u8 = partial(_numpy_to_multiarray, UInt8MultiArray)
-to_numpy_u8 = partial(_multiarray_to_numpy, np.uint8)
+to_numpy_u8 = partial(_multiarray_to_numpy, int, np.uint8)
 to_multiarray_u16 = partial(_numpy_to_multiarray, UInt16MultiArray)
-to_numpy_u16 = partial(_multiarray_to_numpy, np.uint16)
+to_numpy_u16 = partial(_multiarray_to_numpy, int, np.uint16)
 to_multiarray_u32 = partial(_numpy_to_multiarray, UInt32MultiArray)
-to_numpy_u32 = partial(_multiarray_to_numpy, np.uint32)
+to_numpy_u32 = partial(_multiarray_to_numpy, int, np.uint32)
 to_multiarray_u64 = partial(_numpy_to_multiarray, UInt64MultiArray)
-to_numpy_u64 = partial(_multiarray_to_numpy, np.uint64)
+to_numpy_u64 = partial(_multiarray_to_numpy, int, np.uint64)
